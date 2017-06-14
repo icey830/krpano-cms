467ac557b4cd47b62d40362863f0c5b2d15a86b7
vbachev <vasil@vasilbachev.com>
2017-06-14 23:45:43 +0300

    update the hotspot plugin to support CRUD operations

--- KrpanoCMS/Krpano/hotspotpopup/plugin.js
+++ KrpanoCMS/Krpano/hotspotpopup/plugin.js
@@ -1,101 +1,178 @@
-function krpanoplugin() {
+function krpanoplugin () {
 	
 	// the krpano and plugin interface objects
 	var krpano;
 	var plugin;
+	var pluginPath;
 	var layers = [];
-	var i = 0;
+	var api = {
+		isAdmin: false,
+		fetch: function () {},
+		save: function () {},
+		delete: function () {}
+	}
 	
 	// registerplugin - startup point for the plugin (required)
 	// - krpanointerface = krpano interface object
 	// - pluginpath = the fully qualified plugin name (e.g. "plugin[name]")
 	// - pluginobject = the xml plugin object itself
-	this.registerplugin = function(krpanointerface, pluginpath, pluginobject) {
+	this.registerplugin = function (krpanointerface, pluginpath, pluginobject) {
 		krpano = krpanointerface;
 		plugin = pluginobject;
+
+		pluginPath = plugin.url.split('plugin.js')[0];
 		krpano.events.onclick = _handleClick.bind(this);
+
+		_loadConfiguration();
 		_loadStyles();
-		_createHotspots();
+		_loadHotspots();
 	};
 
 	// unloadplugin - exit point for the plugin (optionally)
 	// - will be called from krpano when the plugin will be removed
 	// - everything that was added by the plugin should be removed here
-	this.unloadplugin = function() {
+	this.unloadplugin = function () {
 		plugin = null;
 		krpano = null;
 		layers = null;
 		i = 0;
 		krpano.events.onclick = null;
 	};
-	
-	function _loadStyles() {
-		var styleTag = document.createElement('link');
-	  	styleTag.setAttribute('rel', 'stylesheet');
-	  	styleTag.setAttribute('href', plugin.url.replace('.js', '.css'));
-	  	plugin.sprite.appendChild(styleTag);
+
+	function _loadConfiguration () {
+		if (window.stuff) {
+			api.isAdmin = stuff.admin;
+			api.fetch = typeof stuff.fetch == 'function' ? stuff.fetch : api.fetch;
+			api.save = typeof stuff.save == 'function' ? stuff.save : api.save;
+			api.delete = typeof stuff.delete == 'function' ? stuff.delete : api.delete;
+		} else {
+			console.error('Invalid configuration');
+		}
 	}
 	
-	function _createHotspots() {
-		var data = JSON.parse(krpano.data.getItem(plugin.hotspots).content);
-		data.map(function(item, index) {
-			item.name = 'hotspot-' + index;
-			_createHotspot(item);
+	function _loadStyles () {
+		var styleTag = document.createElement('link');
+		styleTag.setAttribute('rel', 'stylesheet');
+		styleTag.setAttribute('href', pluginPath + 'plugin.css');
+		plugin.sprite.appendChild(styleTag);
+	}
+
+	function _loadHotspots () {
+		api.fetch(function (results) {
+			results.map(function (item, index) {
+				item.name = 'hotspot-' + index;
+				_createHotspot(item);
+			});
 		});
 	}
 	
-	function _createHotspot(data) {
+	function _createHotspot (data) {
 		var hotspot = krpano.addhotspot(data.name);
+		hotspot.id = data.id;
 		hotspot.atv = data.atv;
 		hotspot.ath = data.ath;
 		hotspot.content = data.content;
 		hotspot.distorted = true;
 		hotspot.zorder = 10;
-		hotspot.url = plugin.url.replace('plugin.js', 'pin.png');
-		hotspot.onclick = _openPopup.bind(hotspot);
+		hotspot.url = pluginPath + 'pin.png';
+		hotspot.onclick = _openPopup.bind(this, hotspot);
+		return hotspot;
+	}
+
+	function _hotspotToDTO (hotspot) {
+		return {
+	 		id: hotspot.id,
+	 		ath: hotspot.ath,
+	 		atv: hotspot.atv,
+	 		content: hotspot.content
+	 	};
 	}
 	
-	function _openPopup() {
-		var hotspot = this;
+	function _openPopup (hotspot) {
+		_closeAllPopups();
 		krpano.call("looktohotspot(" + hotspot.name + ")");
 		hotspot.zorder = 20;
-		_closeAllPopups();
 		
 		var layer = krpano.addlayer(hotspot.name);
 		layer.type = 'text';
-		layer.onclick = _closePopup.bind(layer);
 		layer.parent = hotspot.getfullpath();
 		layers.push(layer);
 		
 		var popup = document.createElement('div');
 		popup.classList.add('popup');
-		popup.innerHTML = hotspot.content;
-		
+		_addPopupContent(popup, layer);
 		layer.sprite.appendChild(popup);
 	}
 	
-	function _closePopup(layer) {
-		var target = layer || this;
-		layers.splice(layers.indexOf(target), 1);
-		krpano.get(target.parent).zorder = 10;
-		krpano.removelayer(target.name);
+	function _closePopup (layer) {
+		var hotspot = krpano.get(layer.parent);
+		layers.splice(layers.indexOf(layer), 1);
+		krpano.removelayer(layer.name);
+		hotspot.zorder = 10;
+		if (!hotspot.content) {
+			krpano.removehotspot(hotspot.name);
+		}
 	}
 	
-	function _closeAllPopups() {
-		layers.map(_closePopup)
+	function _closeAllPopups () {
+		layers.map(_closePopup);
+	}
+
+	function _addPopupContent (popup, layer) {
+		var hotspot = krpano.get(layer.parent);
+		if (api.isAdmin) {
+			var field = document.createElement('textarea');
+			field.classList.add('popup-field');
+			field.value = hotspot.content;
+			popup.appendChild(field);
+			_addPopupButton(popup, 'Save', _saveHotspot.bind(null, layer, field));
+			_addPopupButton(popup, 'Cancel', _closePopup.bind(null, layer));
+			if (field.value) {
+				_addPopupButton(popup, 'Delete', _deleteHotspot.bind(null, layer));
+			}
+		} else {
+			popup.innerHTML = hotspot.content;
+		}
+	}
+
+	function _addPopupButton (popup, name, action) {
+		var button = document.createElement('button');
+		button.classList.add('popup-' + name.toLowerCase());
+		button.innerText = name;
+		button.onclick = action;
+		popup.appendChild(button);
+	}
+
+	function _saveHotspot (layer, field) {
+		var hotspot = krpano.get(layer.parent);
+		hotspot.content = field.value;
+	 	api.save(_hotspotToDTO(hotspot), function (recordId) {
+	 		hotspot.id = hotspot.id || recordId;
+	 	});
+	 	_closePopup(layer);
+	}
+
+	function _deleteHotspot (layer) {
+		var hotspot = krpano.get(layer.parent);
+		hotspot.content = '';
+	 	api.delete(hotspot.id);
+	 	_closePopup(layer);
 	}
 	
-	function _handleClick() {
+	function _handleClick () {
+		if (layers.length) {
+			_closeAllPopups();
+			return;
+		}
+		if (!api.isAdmin) return;
 		var coords = krpano.screentosphere(krpano.mouse.x, krpano.mouse.y);
-		var input = prompt('Add content for the hotspot', 'Wow!');
-		if (!input) return;
 		var hotspotData = {
-			name: 'click' + (++i),
+			name: 'click' + Math.round(krpano.timertick),
 			ath: coords.x,
 			atv: coords.y,
-			content: input
+			content: ''
 		};
-		_createHotspot(hotspotData);
-		// @TODO: AJAX POST hotspotData
+		var hotspot = _createHotspot(hotspotData);
+		_openPopup(hotspot);
 	}
 }
