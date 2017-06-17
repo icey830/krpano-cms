ded21cd7fd352e08a7cd9c90227efc71cdfdab02
frvasileva <fanka@klearlending.com>
2017-06-18 00:09:34 +0300

    Tour views added

---
+++ KrpanoCMS/Administration/Controllers/TourController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using KrpanoCMS.Administration.Models;
+using Microsoft.AspNet.Identity;
+
+namespace KrpanoCMS.Administration.Controllers
+{
+    public class TourController : Controller
+    {
+        private Entities db = new Entities();
+
+        public ActionResult Index()
+        {
+            return View(db.Tour.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tour.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        public ActionResult Create()
+        {
+          TourModel model = new TourModel();
+            model.PanoramaList = db.Panorama.ToList()
+                                        .Where(item => item.UserId == User.Identity.GetUserId())
+                                        .ToList();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TourModel tour)
+        {
+            tour.Tour.UserId = User.Identity.GetUserId();
+
+            if (ModelState.IsValid)
+            {
+                db.Tour.Add(tour.Tour);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tour);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Tour tour = db.Tour.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Tour tour)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tour).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tour);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tour.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tour tour = db.Tour.Find(id);
+            db.Tour.Remove(tour);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ KrpanoCMS/Administration/Models/TourModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KrpanoCMS.Administration.Models
+{
+    public class TourModel
+    {
+
+        public Tour Tour { get; set; }
+
+        public List<Panorama> PanoramaList { get; set; }
+
+        public TourModel() { }
+    }
+}
